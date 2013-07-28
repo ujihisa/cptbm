f9c33077638ebd6e5797485f13ad8d76c6cd9b2c
ujihisa <ujihisa@gmail.com>
2013-07-27 18:51:35 -0700

    UX

--- a.js
+++ a.js
@@ -1,5 +1,5 @@
 var x = $('p.text-here').first().text();
 var url = x.match(/http:.*/)[0];
-$(document.body).append('<div id="ujihisa"></div>');
+$(document.body).append('<div id=\'ujihisa\'></div>');
 $('#ujihisa').css('position', 'fixed').css('top', '0px').css('left', '0px').css('background-color', 'white').
-	html('<a href="%s" target=_blank>hi</a>'.replace('%s', url));
+	html('<a href=\'%s\' target=_blank>open recipe</a>'.replace('%s', url));
