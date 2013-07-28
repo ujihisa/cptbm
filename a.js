e5199c55041a971e68d5aa383d4dc11c8fe1522b
ujihisa <ujihisa@gmail.com>
2013-07-27 18:50:14 -0700

    ok

--- a.js
+++ a.js
@@ -1,4 +1,5 @@
 var x = $('p.text-here').first().text();
 var url = x.match(/http:.*/)[0];
+$(document.body).append('<div id="ujihisa"></div>');
 $('#ujihisa').css('position', 'fixed').css('top', '0px').css('left', '0px').css('background-color', 'white').
 	html('<a href="%s" target=_blank>hi</a>'.replace('%s', url));
